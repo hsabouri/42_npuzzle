de25e04346852e44ccbfb29fc3d64892502559f5
Hugo Sabourin <hsabouri@student.42.fr>
2018-02-14 16:17:40 +0100

    tested

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod lib;
 
 fn main() {
-    
+    let mut map = lib::Map::new(3, lib::Cell {x: 0, y: 0}, lib::Cell {x: 2, y: 2});
+    {
+        let mut node_mut = map.get_node_mut(lib::Cell {x: 1, y: 1}).unwrap();
+        
+        node_mut.ctype = lib::CellType::Target;
+        println!("{:#?}", node_mut)
+    }
+    let mut node = map.get_node(lib::Cell {x: 1, y: 1}).unwrap();
+    println!("{:#?}", node)
 }
