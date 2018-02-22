af9e17217e31d67d811c005a864dc733a081e9d3
Hugo Sabourin <hsabouri@student.42.fr>
2018-02-22 19:07:06 +0100

    Beginning final implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,170 +1,135 @@
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Cell {
+extern crate rand;
+use rand::Rng;
+
+#[derive(Debug, Clone)]
+pub enum Movement {
+    Up,
+    Down,
+    Left,
+    Right,
+    No,
+}
+
+#[derive(Debug, Clone)]
+pub struct Point {
     pub x: usize,
     pub y: usize,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum CellType {
-    Clear,
-    Wall,
-    Source,
-    Target,
-}
-
-impl CellType {
-    pub fn figure_out(node: Cell, source: Cell, target: Cell) -> CellType {
-        if node == source {
-            CellType::Source
-        } else if node == target {
-            CellType::Target
-        } else {
-            CellType::Clear
-        }
-    }
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct Node {
-    pub pos: Cell,
-    pub ctype: CellType,
-    pub parent: Option<Cell>,
-    pub nchilds: usize,
-    pub childs: [Option<Cell>; 4],
-    g: Option<f32>,
-    h: Option<f32>,
-    f: Option<f32>,
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Map {
-    content: Vec<Node>,
-    pub size: usize,
-    pub source: Cell,
-    pub target: Cell,
-}
-
-impl Node {
-    pub fn new(pos: Cell, ctype: CellType) -> Node {
-        Node {
-            pos: pos,
-            ctype: ctype,
-            parent: None,
-            nchilds: 0,
-            childs: [None, None, None, None],
-            g: None,
-            h: None,
-            f: None,
-        }
-    }
-
-    pub fn set_parent(&mut self, node: Cell) {
-        if (self.pos.x == node.x && (node.y == self.pos.y - 1 || node.y == self.pos.y + 1)) ||
-           (self.pos.y == node.y && (node.x == self.pos.y - 1 || node.x == self.pos.y + 1)) {
-            self.parent = Some(node);
-        } else {
-            panic!("Assigning not valid parent");
-        }
-    }
-
-    pub fn set_child(&mut self, node: Cell) {
-        if self.nchilds >= 4 {
-            panic!("Trying to assign to many childs to a node");
-        }
-        for child in &self.childs {
-            if child.is_some() && node == child.unwrap() {
-                panic!("Trying to assign an already assigned child");
-            }
-        }
-        if (self.pos.x == node.x && (node.y == self.pos.y - 1 || node.y == self.pos.y + 1)) ||
-           (self.pos.y == node.y && (node.x == self.pos.y - 1 || node.x == self.pos.y + 1)) {
-            self.childs[self.nchilds] = Some(node);
-            self.nchilds += 1;
-        } else {
-            panic!("Assigning not valid child");
-        }
-    }
-
-    pub fn check_child(&self, cell: Cell, map: &Map) -> bool {
-        let node = map.get_node(cell);
-        let mut res = false;
-
-        if node.is_some() &&
-           node.unwrap().ctype != CellType::Wall &&
-           node.unwrap().ctype != CellType::Target &&
-           (self.parent.is_none() || node.unwrap().pos != self.parent.unwrap()) {
-            res = true
-        }
-        res
-    }
-
-    pub fn search_childs(&self, map: &Map) -> Vec<Cell> {
-        let mut res = Vec::<Cell>::new();
-
-        if self.pos.x > 0 {
-            let left = Cell {x: self.pos.x - 1, y: self.pos.y};
-            if self.check_child(left, map) {
-                res.push(left);
-            }
-        }
-        let right = Cell {x: self.pos.x + 1, y: self.pos.y};
-        if self.check_child(right, map) {
-            res.push(right);
-        }
-        if self.pos.y > 0 {
-            let top = Cell {x: self.pos.x, y: self.pos.y - 1};
-            if self.check_child(top, map) {
-                res.push(top);
-            }
-        }
-        let bottom = Cell {x: self.pos.x, y: self.pos.y + 1};
-        if self.check_child(bottom, map) {
-            res.push(bottom);
-        }
-        res
-    }
-
-    pub fn set_childs(&mut self, childs: Vec<Cell>) {
-    }
+    content: Vec<usize>,
+    pos: Point,
+    size: usize,
+    costs: Vec<usize>,
 }
 
 impl Map {
-    pub fn new(size: usize, source: Cell, target: Cell) -> Map {
-        if source.x >= size || source.y >= size {
-            panic!("Source is out of the map");
-        }
-        if target.x >= size || target.y >= size {
-            panic!("Target is out of the map");
-        }
+    pub fn new(content: Vec<usize>, pos: Point, size: usize, costs: Vec<usize>) -> Map {
         Map {
-            content: {(0..size * size).map(|i|{
-                let pos = Cell {x: i % size , y: i / size};
-
-                Node::new(pos, CellType::figure_out(pos, source, target))
-            }).collect()},
+            content: content,
+            pos: pos,
             size: size,
-            source: source,
-            target: target,
+            costs: costs,
+        }
+    }
+    
+    pub fn child(old: &Map, movement: &Movement) -> Map {
+        let mut res = old.clone();
+
+        res.content.swap(old.pos.x + old.pos.y * old.size, {
+            match *movement {
+                Movement::Down => old.pos.x + (old.pos.y - 1) * old.size,
+                Movement::Up => old.pos.x + (old.pos.y + 1) * old.size,
+                Movement::Right => (old.pos.x - 1) + old.pos.y * old.size,
+                Movement::Left => (old.pos.x + 1) + old.pos.y * old.size,
+                Movement::No => old.pos.x + old.pos.y * old.size,
+            }
+        });
+
+        res.pos = match *movement {
+            Movement::Right => Point {x: old.pos.x - 1, y: old.pos.y},
+            Movement::Left => Point {x: old.pos.x + 1, y: old.pos.y},
+            Movement::Down => Point {x: old.pos.x, y: old.pos.y - 1},
+            Movement::Up => Point {x: old.pos.x, y: old.pos.y + 1},
+            Movement::No => Point {x: old.pos.x, y: old.pos.y},
+        };
+        res
+    }
+
+    pub fn gen(size: usize) -> Map {
+        let mut topush: Vec<usize> = (0..(size * size)).collect();
+        let mut pos = Point {x: 0, y: 0};
+        let content: Vec<usize> = (0..(size * size)).map(|map_id: usize| {
+            let id = rand::thread_rng().gen_range(0, topush.len());
+            let res = topush[id];
+
+            topush.remove(id);
+            if res == 0 {
+                pos = Point {x: map_id % size, y: map_id / size};
+            }
+            res
+        }).collect();
+        Map {
+            content: content,
+            pos: pos,
+            size: size,
+            costs: (0..(size * size)).collect(),
         }
     }
 
-    pub fn get_node_mut(&mut self, node: Cell) -> Option<&mut Node> {
-        let size = self.size;
+    pub fn get_solved(&self) -> Map {
+        
+    }
+}
 
-        if node.x >= size || node.y >= size {
-            None
-        } else {
-            Some(&mut self.content[node.x + node.y * size])
+#[derive(Debug, Clone)]
+pub struct Node {
+    map: Map,
+    parent: usize,
+    movement: Movement,
+    hash: usize, // Can basically be a weighted addition of the content and the complexity
+    g: usize,
+    h: usize,
+    f: usize,
+}
+
+impl Node {
+    pub fn new(map: Map, parent: usize, movement: Movement, hash: usize, g: usize, h: usize, f: usize) -> Node {
+        Node {
+            map: map,
+            parent: parent,
+            movement: movement,
+            hash: hash,
+            g: g,
+            h: h,
+            f: f,
         }
     }
 
-    pub fn get_node(&self, node: Cell) -> Option<&Node> {
-        let size = self.size;
+    pub fn child(old: &Node, movement: Movement, parent: usize) -> Node {
+        let random: usize = rand::random::<usize>();
+        Node {
+            map: Map::child(&old.map, &movement),
+            parent: parent,
+            movement: movement,
+            hash: 0, // TODO
+            g: old.g + 1,
+            h: random, // TODO
+            f: old.g + 1 + random, // TODO
+        }
+    }
 
-        if node.x >= size || node.y >= size {
-            None
-        } else {
-            Some(&self.content[node.x + node.y * size])
+    pub fn gen(size: usize) -> (Node, Node) {
+        let random: usize = rand::random::<usize>();
+        Node {
+            map: Map::gen(size),
+            parent: 0,
+            movement: Movement::No,
+            hash: 0, //TODO
+            g: 0,
+            h: random, //TODO
+            f: random, //TODO
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
+extern crate rand;
 mod lib;
 
-fn solve(map: &mut lib::Map) {
-    let source_pos = map.source;
-    let childs = map.get_node(source_pos).unwrap().search_childs(map);
-    let mut source = map.get_node_mut(source_pos).unwrap();
-
-    source.set_childs(childs);
-}
-
 fn main() {
-    let mut map = lib::Map::new(4, lib::Cell {x: 0, y: 0}, lib::Cell {x: 2, y: 2});
-
-    solve(&mut map);
-
-    println!("{:#?}", map);
+    let node = lib::Node::gen(10);
+    let child = lib::Node::child(&node, lib::Movement::Right, 0);
+    println!("{:#?}", node);
+    println!("{:#?}", child);
 }
