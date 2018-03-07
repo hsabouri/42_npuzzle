748fdfe3012b58c92e7e14c02b1a3faa6844fe64
wescande <wescande@student.42.fr>
2018-03-07 12:44:38 +0100

    some clear in bin

--- src/bin/npuzzle.rs
+++ src/bin/npuzzle.rs
@@ -9,16 +9,13 @@ use clap::{Arg, App, ArgMatches};
 
 fn init_map(matches: ArgMatches) -> Result<(Node, Node), &'static str> {
     match matches.value_of("FILE") {
-        Some(filename) => Ok(lib_npuzzle::parse(filename)?),
+        Some(filename) => lib_npuzzle::parse(filename),
         None => {
-            // let size = matches.value_of("SIZE").unwrap_or("3").parse::<u16>().unwrap(); // TODO not an unwrap
             let size = value_t!(matches.value_of("SIZE"), u16).unwrap_or_else(|e| e.exit());
-            if size > 20 {
-                Err("Ah ah nice try. it's too big !.")
-            } else if size < 3 {
-                Err("Size must be equals or higher than 3.")
-            } else {
-                Ok(lib_npuzzle::create_random(size)?)
+            match size {
+                size if size > 20   => Err("Ah ah nice try. it's too big !."),
+                size if size < 3    => Err("Size must be equals or higher than 3."),
+                size                => lib_npuzzle::create_random(size),
             }
         }
     }
@@ -28,14 +25,10 @@ fn main() {
     let matches = App::new("npuzzle")
         .version("1.0")
         .about("Solves n-puzzles")
-        .author("hsabouri")
+        .author("Hugo Sabourin <hsabouri@student.42.fr>, William Escande <wescande@student.42.fr>")
         .arg(Arg::with_name("FILE")
             .help("Input file containing puzzle to solve")
             .index(1))
-        // .arg(Arg::with_name("SIZE")
-        //     .help("Define size of input puzzle")
-        //     .short("s")
-        //     .long("size"))
         .arg(Arg::with_name("SIZE").short("s").long("size").default_value("3").takes_value(true).help("Size of a random generated N-puzzle [3 - 20]"))
         .arg(Arg::with_name("H")
             .help("Heuristic chosen to solve the puzzle")
@@ -51,4 +44,5 @@ fn main() {
         Err(msg)    => println!("{}{}", "Failed to init map: ".red(), msg.red()),
         Ok((map, solved))     => lib_npuzzle::solve(map, solved)
     };
+    //TODO display result depending of verbosity ?
 }
